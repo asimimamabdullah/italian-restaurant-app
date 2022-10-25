88234ccbff706d96b9cefdc48c548fa6e604ed13
asimimamabdullah <asimimamabdullah@gmail.com>
2022-10-26 00:18:48 +0500

    little style change

--- client/src/Components/Screens/Order/Order.jsx
+++ client/src/Components/Screens/Order/Order.jsx
@@ -9,101 +9,96 @@ import { useStateValue } from "../../../StateProvider";
 import Loading from "../Global/Loading";
 
 const Order = () => {
-  const { productsAPI, categoriesAPI } = useStateValue();
-  const [categories] = categoriesAPI.categories;
-  const [products] = productsAPI.products;
-  const navigate = useNavigate();
-  const params = useParams();
+	const { productsAPI, categoriesAPI } = useStateValue();
+	const [categories] = categoriesAPI.categories;
+	const [products] = productsAPI.products;
+	const navigate = useNavigate();
+	const params = useParams();
 
-  const scrollToElement = (name) => {
-    window.scrollTo({
-      top: document.getElementById(name)?.offsetTop - 140,
-      left: 0,
-      behavior: "smooth",
-    });
-  };
+	const scrollToElement = (name) => {
+		window.scrollTo({
+			top: document.getElementById(name)?.offsetTop - 140,
+			left: 0,
+			behavior: "smooth",
+		});
+	};
 
-  useEffect(() => {
-    if (params.categoryName) scrollToElement(params?.categoryName);
-  }, [params?.categoryName]);
+	useEffect(() => {
+		if (params.categoryName) scrollToElement(params?.categoryName);
+	}, [params?.categoryName]);
 
-  document.addEventListener("scroll", () => {
-    const banner = document.querySelector(".order__banner");
-    const header = document.querySelector(".order__header");
+	document.addEventListener("scroll", () => {
+		const banner = document.querySelector(".order__banner");
+		const header = document.querySelector(".order__header");
 
-    if (banner && header && window.scrollY > banner.clientHeight / 2.5 - 1) {
-      header.style.position = "sticky";
-    }
-  });
+		if (banner && header && window.scrollY > banner.clientHeight / 2.5 - 1) {
+			header.style.position = "sticky";
+		}
+	});
 
-  if (!products || !categories) {
-    return <Loading />;
-  }
+	if (!products || !categories) {
+		return <Loading />;
+	}
 
-  return (
-    <>
-      <div className="order">
-        <div className="order__banner">
-          <img
-            src="https://images.pexels.com/photos/3758133/pexels-photo-3758133.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-            alt=""
-          />
-        </div>
-        <div className="order__header">
-          <div className="order__categories">
-            {categories.map((category, i) => (
-              <p
-                className="order__category"
-                key={i}
-                onClick={() => navigate(`/order/${ category?.name }`)}>
-                {category.name}
-              </p>
-            ))}
-          </div>
-        </div>
-        <div className="order__allContainer">
-          {categories?.map((item, i) => (
-            <div key={i} className="order__container-category">
-              <div className="order__container-category-data">
-                <h2
-                  className="order__container-category-text"
-                  id={item?.name}>
-                  {item?.name}
-                </h2>
-                <img
-                  src={item?.images?.url}
-                  width="100%"
-                  height="150"
-                  alt=""
-                />
-              </div>
+	return (
+		<>
+			<div className="order">
+				<div className="order__banner">
+					<img
+						src="https://images.pexels.com/photos/3758133/pexels-photo-3758133.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+						alt=""
+					/>
+				</div>
+				<div className="order__header">
+					<div className="order__categories">
+						{categories.map((category, i) => (
+							<p
+								className={`order__category ${
+									params.categoryName === category?.name ? "order__category-bottom-border" : ""
+								}`}
+								key={i}
+								onClick={() => navigate(`/order/${category?.name}`)}>
+								{category.name}
+							</p>
+						))}
+					</div>
+				</div>
+				<div className="order__allContainer">
+					{categories?.map((item, i) => (
+						<div key={i} className="order__container-category">
+							<div className="order__container-category-data">
+								<h2 className="order__container-category-text" id={item?.name}>
+									{item?.name}
+								</h2>
+								<img src={item?.images?.url} width="100%" height="150" alt="" />
+							</div>
 
-              <div className="order__container">
-                {products
-                  .filter((cat) => cat.category === item.name)
-                  .map((p, i) => {
-                    return (
-                      <Product
-                        key={i}
-                        id={p.product_id}
-                        title={p.title}
-                        description={p.description}
-                        price={p.price}
-                        image={p.images.url}
-                      />
-                    );
-                  })}
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="order__right-section-order">
-        <OrderTotal />
-      </div>
-      {(!products || products.length === 0) && <Loading />}
-    </>
-  );
+							<div className="order__container">
+								{products
+									.filter((cat) => cat.category === item.name)
+									.map((p, i) => {
+										return (
+											<Product
+												key={i}
+												id={p.product_id}
+												title={p.title}
+												description={p.description}
+												price={p.price}
+												image={p.images.url}
+											/>
+										);
+									})}
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+			<div className="order__right-section-order">
+				<OrderTotal />
+			</div>
+			{(!products || products.length === 0) && <Loading />}
+		</>
+	);
 };
 
 export default Order;
